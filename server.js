e40f092bf7ed29273b28d86c6b38ddf7944fe587
Tharnslair <jsmith33@ittesi.com>
2014-10-09 16:07:27 -0400

    done with layout

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// required files
-
-var express = require('express');
-    stylus = require('stylus');
-    logger = require('morgan');
-    bodyParser = require('body-parser');
-
-// Basic config
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -14,8 +11,6 @@ var app = express();
 function compile(str, path) {
     return stylus(str).set('filename', path);
 }
-
-// Express config
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
@@ -27,21 +22,29 @@ app.use(stylus.middleware(
         compile: compile
     }
 ));
+app.use(express.static(__dirname + '/public'));
 
-// Routes
+//mongoose.connect('mongodb://localhost/multivision');
+//var db = mongoose.connection;
+//db.on('error', console.error.bind(console, 'connection error...'));
+//db.once('open', function callback() {
+//    console.log('multivision db opened');
+//});
+//var messageSchema = mongoose.Schema({message: String});
+//var Message = mongoose.model('Message', messageSchema);
+//var mongoMessage;
+//Message.findOne().exec(function(err, messageDoc) {
+//    mongoMessage = messageDoc.message;
+//});
+//
+//app.get('/partials/:partialPath', function(req, res) {
+//    res.render('partials/' + req.params.partialPath);
+//});
 
 app.get('*', function(req, res) {
     res.render('index');
 });
 
-// static routing to public directory
-
-app.use(express.static(__dirname + '/public'));
-
-// listen on the port....
-
-var port = 1130;
-
+var port = 3030;
 app.listen(port);
-
-console.log('Listening on port.... ' + port + '.....');
+console.log('Listening on port ' + port + '...');
