5bfe999a34528936b40131a7e9758936a90be814
Tharnslair <jsmith33@ittesi.com>
2014-10-09 10:48:16 -0400

    done with stylus

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-/**
- * Created by 16848519 on 10/8/2014.
- */
-
 // required files
+
 var express = require('express');
+    stylus = require('stylus');
+    logger = require('morgan');
+    bodyParser = require('body-parser');
+
+// Basic config
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-// config
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+// Express config
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
 
 // Routes
 
 app.get('*', function(req, res) {
     res.render('index');
 });
+
+// static routing to public directory
+
+app.use(express.static(__dirname + '/public'));
 
 // listen on the port....
 
